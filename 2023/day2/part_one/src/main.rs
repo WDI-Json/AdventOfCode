6e2886cfbb8648898c95a7c63ccb45a64995d50e
Wouter Dijks <wouter.dijks@tauw.com>
2023-12-05 19:06:53 +0100

    wip but working check

--- 2023/day2/part_one/src/main.rs
+++ 2023/day2/part_one/src/main.rs
@@ -12,7 +12,10 @@ fn sum_of_valid_games(filename: &str) -> i32 {
     let input_strings = contents.split("\n");
     
     //Cleanup strings
-
+    for line in input_strings {
+        let line_hashmap = parse_game_data(line);
+        
+    }
     //
     0
 }
@@ -35,38 +38,31 @@ fn parse_game_data(game_data: &str) -> HashMap<String, Vec<HashMap<&str, u32>>> 
     result
 }
 
+fn is_round_valid(hashmap: &HashMap<String, Vec<HashMap<&str, u32>>>) -> bool {
+    let rows = hashmap.get("Game 1").unwrap();
+    println!("isround {:?}", hashmap.keys());
+    for row in rows {
+        let red = row.get("red");
+        let green = row.get("green");
+        let blue = row.get("blue");
+        //only 12 red cubes, 13 green cubes, and 14 blue cubes
+        if red > Some(&12) || green > Some(&13) || blue > Some(&14) {
+            return false;
+        }
+    }
+    true
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::iter::FromIterator;
 
     #[test]
     fn test_parse_game_data() {
         let game_data = "Game 1: 13 green, 3 red; 4 red, 9 green, 4 blue; 9 green, 10 red, 2 blue";
         let game_dict = parse_game_data(game_data);
-
-        let mut expected = HashMap::new();
-        expected.insert("Game 1".to_string(), vec![
-            HashMap::from([("green", 13), ("red", 3), ("blue", 0)]),
-            HashMap::from([("green", 9), ("red", 4), ("blue", 4)]),
-            HashMap::from([("green", 9), ("red", 10), ("blue", 2)])
-        ]);
-
-        let rows = game_dict.get("Game 1").unwrap();
-        for row in rows {
-            let red = row.get("red");
-            let green = row.get("green");
-            let blue = row.get("blue");
-            //only 12 red cubes, 13 green cubes, and 14 blue cubes?
-            if red > Some(&12) || green > Some(&13) || blue > Some(&14) {
-                break;
-            }
-        }
-
-        for row in rows {
-            println!("{:?}", row);
-        }
+        assert_eq!(is_round_valid(&game_dict), true);
 
         // Optionally, print the result (note that println! in tests only shows output when the test fails or when run with `cargo test -- --nocapture`)
         println!("Test passed. Game dictionary: {:?}", game_dict);
