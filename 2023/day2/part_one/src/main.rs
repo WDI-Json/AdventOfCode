6a1d89c90974a8baf1387ab0d7770142c8542420
Wouter Dijks <wouter.dijks@tauw.com>
2023-12-06 08:56:13 +0100

    part one complete

--- 2023/day2/part_one/src/main.rs
+++ 2023/day2/part_one/src/main.rs
@@ -6,18 +6,18 @@ fn main() {
     println!("Sum of valid games: {}", valid_games);
 }
 
-fn sum_of_valid_games(filename: &str) -> i32 {
-    //Grab file from dirpath
+fn sum_of_valid_games(filename: &str) -> u32 {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let input_strings = contents.split("\n");
-    
-    //Cleanup strings
+    let input_strings = contents.lines();
+
+    let mut total_sum = 0;
     for line in input_strings {
         let line_hashmap = parse_game_data(line);
-        
+        if let Some(game_number) = is_round_valid(&line_hashmap) {
+            total_sum += game_number;
+        }
     }
-    //
-    0
+    total_sum
 }
 fn parse_game_data(game_data: &str) -> HashMap<String, Vec<HashMap<&str, u32>>> {
     let mut result = HashMap::new();
@@ -38,19 +38,22 @@ fn parse_game_data(game_data: &str) -> HashMap<String, Vec<HashMap<&str, u32>>> 
     result
 }
 
-fn is_round_valid(hashmap: &HashMap<String, Vec<HashMap<&str, u32>>>) -> bool {
-    let rows = hashmap.get("Game 1").unwrap();
-    println!("isround {:?}", hashmap.keys());
-    for row in rows {
-        let red = row.get("red");
-        let green = row.get("green");
-        let blue = row.get("blue");
-        //only 12 red cubes, 13 green cubes, and 14 blue cubes
-        if red > Some(&12) || green > Some(&13) || blue > Some(&14) {
-            return false;
+fn is_round_valid(hashmap: &HashMap<String, Vec<HashMap<&str, u32>>>) -> Option<u32> {
+    let game_key = hashmap.keys().next().unwrap();
+    let game_number = game_key.trim().split(' ').nth(1).unwrap().parse::<u32>().unwrap();
+
+    if let Some(rows) = hashmap.get(game_key) {
+        for row in rows {
+            let red = *row.get("red").unwrap_or(&0);
+            let green = *row.get("green").unwrap_or(&0);
+            let blue = *row.get("blue").unwrap_or(&0);
+            //only 12 red cubes, 13 green cubes, and 14 blue cubes
+            if red > 12 || green > 13 || blue > 14 {
+                return None;
+            }
         }
     }
-    true
+    Some(game_number)
 }
 
 
@@ -62,7 +65,7 @@ mod tests {
     fn test_parse_game_data() {
         let game_data = "Game 1: 13 green, 3 red; 4 red, 9 green, 4 blue; 9 green, 10 red, 2 blue";
         let game_dict = parse_game_data(game_data);
-        assert_eq!(is_round_valid(&game_dict), true);
+        assert_eq!(is_round_valid(&game_dict), Some(1));
 
         // Optionally, print the result (note that println! in tests only shows output when the test fails or when run with `cargo test -- --nocapture`)
         println!("Test passed. Game dictionary: {:?}", game_dict);
